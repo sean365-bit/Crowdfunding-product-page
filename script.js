dc898b020062348f1e92022fd073c719b73be16c
Cesar <cerh_dev@outlook.com>
2025-04-01 18:54:58 -0600

    add menu modal

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("this is a scss project");
+
+const menuButton = document.querySelector(".menuButton");
+const modal = document.getElementById("myModal");
+const closeModal = document.getElementById("close");
+
+menuButton.addEventListener("click", openMenu);
+closeModal.addEventListener("click", closeMenu);
+
+function openMenu() {
+  modal.classList.add("active");
+  menuButton.classList.add("close");
+}
+
+function closeMenu() {
+  modal.classList.remove("active");
+  menuButton.classList.remove("close");
+}
