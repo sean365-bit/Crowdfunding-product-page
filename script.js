9a7c30af5bb8b7da6691d3d2739e2aeaef88588e
Cesar <cerh_dev@outlook.com>
2025-04-03 18:00:50 -0600

    add bookmark button

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ console.log("this is a scss project");
 const menuButton = document.querySelector(".menuButton");
 const modal = document.getElementById("myModal");
 const closeModal = document.getElementById("close");
+const bookMark = document.querySelector(".bookmark");
+
+const bookMarkButton = document.getElementById("circle");
+const path = document.getElementById("path");
+const bookMarkText = document.getElementById("bookmarkText");
 
 menuButton.addEventListener("click", openMenu);
 closeModal.addEventListener("click", closeMenu);
+bookMark.addEventListener("click", bookMarkClicked);
 
 function openMenu() {
   modal.classList.add("active");
@@ -15,4 +21,10 @@ function openMenu() {
 function closeMenu() {
   modal.classList.remove("active");
   menuButton.classList.remove("close");
+}
+
+function bookMarkClicked() {
+  bookMarkButton.classList.toggle("circle");
+  path.classList.toggle("path");
+  bookMarkText.classList.toggle("bookmark_text");
 }
